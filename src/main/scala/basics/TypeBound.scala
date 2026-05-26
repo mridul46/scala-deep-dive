a4e8448f697e7991fe2f5eb72de8331949053f1c
mridul <mridulvai46@gmail.com>
2026-05-26 14:44:45 +0530

    add TypeBound and Type Hierarchy

---
+++ src/main/scala/basics/TypeBound.scala
@@ -0,0 +1,65 @@
+package basics
+
+//Type Bound in Scala is like a rule. Type Bound limits what a parameter and variable can be. You can set the limits for the variables in the Scala.
+/*
+  Types of Type Bounds
+  1.UPPER TYPE BOUND (T <: A)
+     - An upper type bound limits a type variable to be a subtype of a specific type. It uses the <: operator.
+     -T <: A means: "Type T can be A or any subclass of A."
+  2.LOWER TYPE BOUND (T >: A)
+    - A lower type bound limits a type variable to be a supertype of a specific type. It uses the >: operator.
+    - T >: A means: "Type T must be A or a superclass/parent of A."
+ */
+//
+//object  TypeBound extends App {
+//
+//  trait  Crop {
+//    def name:String
+//  }
+//
+//  case class Rice(name:String,waterRequirementLitres:Int ) extends Crop
+//  case class Potato(name:String,soilType:String) extends Crop
+//  case class Vehicle(model:String)  //Not Crop
+//
+//  /*
+//    - Now, you want to build a CropHarvester class that can process crops. If you make it completely generic ([T]), someone could accidentally pass a Vehicle into it. If you don't use generics at all, you lose the specific type information of what is returned.
+//
+//    - The Solution: Upper Bound
+//    -By using T <: Crop, you guarantee that whatever type is passed into the harvester has access to the Crop methods (like .name), while preventing invalid classes from being used.
+//
+//   */
+//  // T must be a subtype of Crop
+//  class CropHarvester[T <: Crop] {
+//    def inspect(crop: T): Unit = {
+//      println(s"Inspecting crop: ${crop.name}")
+//      // Compiles perfectly because the compiler knows T has a .name method
+//    }
+//  }
+//
+//  // --- EXECUTION CODE ---
+//  // Now these will actually execute and print when you run the object!
+//  val riceHarvester = new CropHarvester[Rice]
+//  val potatoHarvester = new CropHarvester[Potato]
+// // val carHarvester = new CropHarvester[Vehicle]
+//  // ❌ Compile Error: Type Vehicle does not conform to bound Crop
+//
+//  riceHarvester.inspect(Rice("Winter Rice", 1500))
+//  potatoHarvester.inspect(Potato("Local Potato", "Alluvial Soil"))
+//
+//}
+object TypeBound {
+
+  trait Crop { def name: String }
+  case class Rice(name: String, waterRequirementLitres: Int) extends Crop
+  case class Potato(name: String, soilType: String) extends Crop
+
+  class CropHarvester[T <: Crop] {
+    def inspect(crop: T): Unit = println(s"Inspecting crop: ${crop.name}")
+  }
+
+  // 👈 Scala looks for this exact method signature to trigger the run
+  def main(args: Array[String]): Unit = {
+    val riceHarvester = new CropHarvester[Rice]
+    riceHarvester.inspect(Rice("Autumn Rice", 1200))
+  }
+}
---
+++ src/main/scala/basics/TypeHierarchy.scala
@@ -0,0 +1,120 @@
+package basics
+/*
+In Scala, Type Hierarchy means how all data types are organized in a parent-child structure.
+                     Any
+                   /   \
+               AnyVal  AnyRef
+                 |        |
+      ----------------    |
+      |  |  |  |  |       |
+    Int Double Float ... Object Types
+                           |
+                        String,
+                        List,
+                        Array etc.
+
+                 Null
+                   |
+                Nothing
+ */
+object TypeHierarchy {
+  def main(args:Array[String]): Unit = {
+
+
+    /*
+       1.  Any (Top Parent Type)
+       -Any is the root of all types in Scala
+       -Everything in Scala inherits from Any.
+     */
+    val a:Any=10;
+    val b:Any="Hello"
+    val c:Any= true
+    println(s"the result are ${a}, ${b}, ${c}")
+    //Any contains common methods like: .equal(), .hashCode(),.toString()
+
+
+
+   /*
+    2. AnyVal (Value Types)
+     -AnyVal stores primitive/value data.
+     -These are fast and memory efficient.
+     -Types under AnyVal:
+
+     Int
+     Double
+     Float
+     Long
+     Short
+     Byte
+     Char
+     Boolean
+     Unit
+    */
+
+    val num:Int =100;
+    val pi:Double=3.14
+    val flag:Boolean=false
+    println(s"result will be $num $pi $flag")
+
+    /*
+     3.AnyRef - Reference Types
+      -all classes and objects come under AnyRef
+      -Example: -String,
+                -List
+                -Array
+                -User defined classes
+     */
+
+    val name:String="Mridul Hussain"
+    println(name.length)
+    println(name.toUpperCase)
+
+    //User defined class
+    class Student(val name:String, val age:Int)
+    val s1=new Student("Mridul",11)
+    println(s1.name)
+    println(s1.age)
+
+
+
+    //4. Null - it works only with reference types
+     val s:String =null
+     println(s)
+
+    //Error because Int belongs to AnyVal
+    //val x:Int=null
+    /*
+     Why scala avoids null
+     scala prefers:
+     Option
+     Some
+     None
+     */
+    //Safer approach
+    val name1:Option[String]=None
+    println(name1)
+
+    /*
+    5. Nothing ->Bottom Type
+       Nothing is below every type
+       Used for:
+       -Exceptions
+       -infinite loops
+       -failed computations
+     */
+
+    def divide(a: Double, b: Double): Double = {
+      if (b == 0) throw new ArithmeticException("Divide by Zero")
+      else (a / b)
+    }
+    println(divide(3,1))
+
+    /*
+    6. Unit ->Equivalent to Java void.
+     */
+    def greet():Unit={
+      println("Welcome")
+    }
+    greet()
+  }
+}
