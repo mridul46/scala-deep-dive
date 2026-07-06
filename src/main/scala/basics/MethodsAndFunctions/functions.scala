d3a8c6c0b8385b829e183c2f6687841bfaaf18a5
mridul <mridulvai46@gmail.com>
2026-07-06 11:37:25 +0530

    learn collections basic

--- src/main/scala/basics/MethodsAndFunctions/functions.scala
+++ src/main/scala/basics/MethodsAndFunctions/functions.scala
@@ -4,6 +4,8 @@ object functions {
   def main (args:Array[String]):Unit ={
     val add = (a: Int, b: Int) => a + b
     val sum = add(5, 3)
-    println(s"The sum is: $sum")
+    println(s"The sum is: $sum mridul")
   }
 }
+
+
